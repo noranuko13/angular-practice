51f64bfc705b0b86906c3e3c429627a256d59679
noranuko13 <noranuko13@gmail.com>
2019-07-15 20:00:41 +0900

    Create users
    npm run ng -- generate interface interfaces/user

--- src/app/in-memory-web-api.service.ts
+++ src/app/in-memory-web-api.service.ts
@@ -1,9 +1,29 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import * as faker from 'faker/locale/ja';
+
+import { User } from './interfaces/user';
+
+const oneUser = (): User => {
+  return {
+    id: faker.random.number(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+  };
+};
+const manyUsers = (count = 5): User[] => {
+  const users: User[] = [];
+  for (let i = 0; i < count; i++) {
+    users.push(oneUser());
+  }
+  return users;
+};
 
 export class InMemoryWebApiService implements InMemoryDbService {
 
   createDb() {
-    return {};
+    return {
+      users: manyUsers(),
+    };
   }
 
 }
