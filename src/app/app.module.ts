a256fdff39ef5be1a183298d7557c1907bdad13c
noranuko13 <noranuko13@gmail.com>
2019-07-15 18:52:41 +0900

    Import the in-memory web api module
    npm run ng -- generate service in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { InMemoryWebApiService } from './in-memory-web-api.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,6 +17,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    environment.production ?
+      [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryWebApiService, { delay: 100 }),
     AppRoutingModule,
     SharedModule,
   ],
---
+++ src/app/in-memory-web-api.service.ts
@@ -0,0 +1,9 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryWebApiService implements InMemoryDbService {
+
+  createDb() {
+    return {};
+  }
+
+}
