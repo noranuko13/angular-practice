c0f879eda808d28a05713e5378a1844a0609899b
noranuko13 <noranuko13@gmail.com>
2019-07-17 21:29:07 +0900

    npm install @angular/flex-layout --save

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
 import { LayoutModule } from './layout/layout.module';
 
@@ -9,6 +10,7 @@ import { LayoutModule } from './layout/layout.module';
   ],
   exports: [
     LayoutModule,
+    FlexLayoutModule,
     MaterialModule
   ]
 })
