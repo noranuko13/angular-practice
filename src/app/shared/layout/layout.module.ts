cb06d62cb30f897226d878a63425f6f5756b576d
noranuko13 <noranuko13@gmail.com>
2019-07-22 20:35:46 +0900

    Move layout components

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '../material/material.module';
-import { LayoutHeaderComponent } from './components/layout-header/layout-header.component';
-import { LayoutFooterComponent } from './components/layout-footer/layout-footer.component';
+import { LayoutHeaderComponent } from './layout-header/layout-header.component';
+import { LayoutFooterComponent } from './layout-footer/layout-footer.component';
 
 const components = [
   LayoutHeaderComponent,
