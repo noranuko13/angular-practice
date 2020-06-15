6a010bd343c764dc332133f7194199a84fea7c08
noranuko13 <noranuko13@gmail.com>
2020-06-15 18:49:56 +0900

    Fixed the initial generated test.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display "Angular Practice"', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-practice!');
+    expect(page.getText('app-layout-header span.site-header-title')).toEqual('Angular Practice');
   });
 
   afterEach(async () => {
