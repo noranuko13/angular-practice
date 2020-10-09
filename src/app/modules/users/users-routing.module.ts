f9ff52bea24b88e93959951f22d71e78119556a1
noranuko13 <noranuko13@gmail.com>
2020-10-10 08:46:09 +0900

    :notes: [Users] Add search routing settings.

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersBaseComponent } from './pages/users-base.component';
+import { UsersSearchComponent } from './pages/users-search/users-search.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersBaseComponent,
+    children: [
+      { path: 'search', component: UsersSearchComponent },
+    ],
   }
 ];
 
