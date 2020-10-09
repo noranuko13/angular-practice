6ec69c228ac44cbd8e9b4dac6690da7786b8e9ef
noranuko13 <noranuko13@gmail.com>
2020-10-10 08:42:13 +0900

    :zap: [Users] ng generate component -s modules/users/pages/users-search

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersBaseComponent } from './pages/users-base.component';
+import { UsersSearchComponent } from './pages/users-search/users-search.component';
 
 
 @NgModule({
   declarations: [
     UsersBaseComponent,
+    UsersSearchComponent,
   ],
   imports: [
     CommonModule,
