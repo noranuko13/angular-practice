2fdda7f863c51809c977ad768afe558c758aeca3
noranuko13 <noranuko13@gmail.com>
2020-10-10 08:33:10 +0900

    :zap: [Users] ng generate component -s modules/users/pages/users-base --flat

---
+++ src/app/modules/users/pages/users-base.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-base',
+  templateUrl: './users-base.component.html',
+  styles: [
+  ]
+})
+export class UsersBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
