c72479fb2e32a34de2dfbf9af30d789aa557f16c
noranuko13 <noranuko13@gmail.com>
2020-10-10 14:47:29 +0900

    :notes: [Users] Implement UsersService.getUsers().

--- src/app/modules/users/users.service.spec.ts
+++ src/app/modules/users/users.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+
+import { UserInterface as IUser } from 'src/app/interfaces/user.interface';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
+  let spy: { get: jasmine.Spy };
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+    });
+    spy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new UsersService(spy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getUsers', () => {
+    const expectedUsers: IUser[] = [
+      { id: 38857, firstName: '蒼空', lastName: '佐藤' },
+      { id: 97587, firstName: '美咲', lastName: '佐々木' },
+    ];
+    spy.get.and.returnValue(of(expectedUsers));
+
+    service.getUsers().subscribe(
+      users => expect(users).toEqual(expectedUsers, 'Get expected users.'),
+      fail
+    );
+    expect(spy.get.calls.count()).toBe(1, 'HttpClient called once.');
   });
 });
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { UserInterface  as IUser } from 'src/app/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>('api/users');
+  }
 
 }
