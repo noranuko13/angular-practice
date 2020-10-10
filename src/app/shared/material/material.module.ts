ed5b8ea8af122a135c184c8ed7d64cef9d370482
noranuko13 <noranuko13@gmail.com>
2020-10-10 15:03:35 +0900

    :notes: [Users] Import MatTableModule.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
   MatButtonModule,
+  MatTableModule,
   MatToolbarModule,
 ];
 
