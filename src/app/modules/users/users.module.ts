4dd7ac20a4d38434dd4b2fd8087c102a60d074f5
noranuko13 <noranuko13@gmail.com>
2020-10-10 15:03:35 +0900

    :notes: [Users] Import MatTableModule.

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersBaseComponent } from './pages/users-base.component';
 import { UsersSearchComponent } from './pages/users-search/users-search.component';
@@ -15,6 +17,7 @@ import { UsersListComponent } from './partials/users-list/users-list.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     UsersRoutingModule
   ]
 })
